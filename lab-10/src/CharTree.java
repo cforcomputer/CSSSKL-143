93fed1c1222e081a80b2eb8a12a0cf684f638196
cforcomputer <patjobri@gmail.com>
2019-12-11 22:51:32 -0800

    Completed the first section

--- lab-10/src/CharTree.java
+++ lab-10/src/CharTree.java
@@ -1,12 +1,14 @@
-import java.io.*;
-import java.util.*;
+import com.sun.source.tree.Tree;
+
+import javax.swing.tree.TreeNode;
+import java.sql.PreparedStatement;
 
 /**
  * Class invariant: This code for a binary tree satisfies the
  * binary search tree storage rule.
  * CSSSKL 143
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Patrick O'Brien
+ * @version 12/8/2019
 /**
  */
 
@@ -18,17 +20,50 @@ import java.util.*;
       * showElementsInSubtree. This class should have more methods. 
       * This is just a sample of possible methods.
       */
+
+
      private static class TreeNode {
-	 
-		// Declare private data type char
-        // Declare 2 links, rightLink & leftLink of type TreeNode
-       
-         
-         // Parametrized constructor to build a node
-         public TreeNode(char newData, TreeNode newLeftLink, TreeNode newRightLink) {
-		    // complete the constructor
-             
+
+         // Declare private data type char
+         // Declare 2 links, rightLink & leftLink of type TreeNode
+         private char data;
+         private TreeNode leftLink;
+         private TreeNode rightLink;
+
+         // Default constructor to create a new tree
+         public TreeNode(char newData) {
+             data = newData;
+             leftLink = null;
+             rightLink = null;
          }
+         
+        // Parametrized constructor to build a node
+        public TreeNode(char newData, TreeNode newLeftLink, TreeNode newRightLink) {// complete the constructor
+             data = newData;
+             leftLink = newLeftLink;
+             rightLink = newRightLink;
+        }
+
+        /**
+         * Getter for data
+         * */
+        public char getData() {
+            return data;
+        }
+
+        /**
+         * Getter for left TreeNode
+         * */
+        public TreeNode getLeftLink() {
+            return leftLink;
+        }
+
+        /**
+         * Getter for right TreeNode
+         * */
+        public TreeNode getRightLink() {
+            return rightLink;
+        }
      }           //End of IntTreeNode inner class
      
      // The first node of the tree, called root
@@ -52,7 +87,6 @@ import java.util.*;
      public void showElements( ) {
          showElementsInSubtree(root);
      }
-     
      /**
      Returns the root node of a tree that is the tree with root node
      subTreeRoot, but with a new node added that contains item.
@@ -72,15 +106,24 @@ import java.util.*;
      
      private static boolean isInSubtree(char item, TreeNode subTreeRoot) {
 		// base case: is subTreeRoot null?    then return false
-		
-		// else if subTreeRoot.data == item   what would you return?
-       
-        // else item < subTreeRoot.data
-			// recursive call
-				
-         //else         // item >= link.data
-            // recursive call
+		if (subTreeRoot == null) {
+		    return false;
         }
+		// else if subTreeRoot.data == item   what would you return?
+       else if (subTreeRoot.data == item) {
+           return true;
+        }
+        // else item < subTreeRoot.data
+		// recursive call
+		else if (item < subTreeRoot.data) {
+		    isInSubtree(item, subTreeRoot.leftLink);
+            //else         // item >= link.data
+            // recursive call
+        } else {
+		    isInSubtree(item, subTreeRoot.rightLink);
+        }
+		return false;
+     }
         
      private static void showElementsInSubtree(TreeNode subTreeRoot) { //Uses inorder traversal.
          if (subTreeRoot != null) {
@@ -97,7 +140,87 @@ import java.util.*;
         tree.add('a');
         tree.add('t');
         tree.add('s');
+
+        // provided driver code
+        System.out.println(tree.toString(tree.root)); // inorder prints a c t
+        System.out.println(tree.countNodes(tree.root));
+        System.out.println(tree.getDepth(tree.root));
+        System.out.println(tree.getParent('a', tree.root)); // can be tricky
         showElementsInSubtree(tree.root);
-		
+
     }
- }
+
+    /**
+     * The getParent method returns the parent root using recursion
+     * @param a = char
+     * @param p = TreeNode
+     * */
+    private TreeNode getParent(char a, TreeNode p) {
+        if (this.root == null) {
+            return null;
+        }
+
+        // keep track of the current root
+        CharTree currentRoot = new CharTree();
+
+        // if the right or left link are equal to a
+        if (p.getLeftLink().data == a || p.getRightLink().data == a) {
+            return this.root; // return the current root as parent
+        }
+
+        // else if the current root is less than the previous node
+        else {
+            if (p.data < root.data) {
+                return getParent(a, p.getRightLink());
+            } else {
+                return getParent(a, p.getLeftLink());
+            }
+        }
+     }
+
+     /**
+      * The getDepth method returns the max depth of the BST
+      *
+      * */
+    private int getDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = getDepth(root.leftLink);
+        int rightDepth = getDepth(root.rightLink);
+
+        int bigger = Math.max(leftDepth, rightDepth);
+
+        return bigger + 1;
+    }
+
+    /**
+     * This method returns the total number of nodes as an int
+     * */
+    private int countNodes(TreeNode root) {
+         if (root == null) {
+             return 0;
+         }
+         int count = 1;
+
+         count += countNodes(root.leftLink);
+         count += countNodes(root.rightLink);
+         return count;
+    }
+
+    /**
+     * This method returns the array represented visually
+     * as a string
+     * */
+    public String toString(TreeNode root) {
+        String result = "";
+        if (root == null) {
+            return "The BST is empty";
+        }
+        result += toString(root.getLeftLink());
+        result += toString(root.getLeftLink());
+        result += root.data;
+        return result;
+    }
+
+}
--- lab-10/src/MorseTree.java
+++ lab-10/src/MorseTree.java
@@ -1,131 +1,162 @@
-﻿import java.io.File;
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.*;;
-/*
+import java.util.*;
+/**
  * MorseTree.java
  * CSSSKL 143 Binary Search Tree Lab
  * Author: Rob Nash
- * 
- * This class reads in data from a text file ("data.txt") and populates a binary tree with an 
- * ordering constraint.  See the lab instructions for more information, but in general, dots go right 
+ *
+ * This class reads in data from a text file ("data.txt") and populates a binary tree with an
+ * ordering constraint.  See the lab instructions for more information, but in general, dots go right
  * and dashes go left when constructing or traversing a Morse code tree.  Search for //TODO
  * in the code to see what code you have to implement.
- * 
- * Start with the constructor. In your constructor read each line in from the textfile first, 
+ *
+ * Start with the constructor. In your constructor read each line in from the textfile first,
  * calling add() for each {letter, morseCodeStr} pair.
- * 
+ *
  */
-
 public class MorseTree {
-    //TODO: data member called "root" goes here
-    
-    
     //TODO: Complete constructor
     public MorseTree() {
-       
-		//first, open data.txt, add each line to the tree
-		Scanner fin;
 		try {
-			//for each line in the file, 
-			//  get the letter(char) and the Morse string
-			//  call add() with this data
-			//  print out the letter and Morse string here for debugging
-		
+            //first, open data.txt, add each line to the tree
+            File text = new File("data.txt");
+            Scanner fin = new Scanner(text);
+
+            int lineNumber = 1; //counter for # of lines if needed
+            while (fin.hasNextLine()) {
+
+                //for each line in the file,
+                //  get the letter(char) and the Morse string
+                //  call add() with this data
+                //  print out the letter and Morse string here for debugging
+
+                String line = fin.nextLine().trim();
+                if (line.length() > 0) {
+                    add(line.substring(1).trim(), line.charAt(0));
+                    System.out.println("Added [ " + line.substring(1).trim()
+                            + " = " + line.charAt(0) + " ] to the tree");
+                }
+                lineNumber++;
+            }
+            fin.close();
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-    
-    
+	//TODO
+    private TreeNode root;
+
+
     public void add(String morseStr, char letter) {
         root = insertInSubtree(morseStr, letter, root);
     }
-    
+
     //TODO: recursively complete this function.  It's only a few characters different from findInSubtree()
 	private TreeNode<Character> insertInSubtree(String morseStr, char letter, TreeNode subtree) {
-		//base case 1 : subtree is null
+
+        /*
+         * Note: public add function has been provided
+         * Walk the tree while morseStr.length() > 0, removing the leading character from the morse
+         * string and ...
+         *  - Create a new TreeNode if your subtree is null
+         *  - Recursively move down the tree, right right if a "m" and left if a "-".
+         * */
+
+
+        //base case 1 : subtree is null
 		//base case 2 : morseStr is of length 0
 		//recursive case 1: the first char in morseStr is a '.', so recursively traverse tree
 		//recursive case 2: the first char in the morseStr is a '-', so recurse accordingly
-		
+        if (subtree == null) {
+            return TreeNode<>
+        }
+
+        while (morseStr.length() > 0) {
+            // remove the leading character from the string
+        }
+
 		return subtree;  //always the last line, always return the node you are working on
 	}
-    
+
     public Character translate(String morseStr) {
         return findInSubtree(morseStr, root);
     }
-    
+
     //TODO: recursively complete this function.  Very similar to insertInSubtree()
 	private Character findInSubtree(String morseStr, TreeNode subtree) {
-		//base case 1 : subtree is null 
+		//base case 1 : subtree is null
 		//base case 2 : morseStr is of length 0
 		//recursive case 1: the first char in morseStr is a '.', so recursively traverse tree
 		//recursive case 2: the first char in the morseStr is a '-', so re-curse accordingly
 		return null;  //remove this
 	}
-    
+
     //TODO: Non-recursive function that calls other (recursive) functions
 	public String translateString(String tokens) {
 		String retVal = "";
 		//build a scanner here using tokens as input
 		//iterate over the tokens calling translate on each token (substring separated by a space)
 		//concat these characters and return them
-		
+
 		return retVal;
 	}
-	
+
     public String toMorseCode(Character c) {
-        .
+
         //walk the tree looking for the TreeNode with the char c in it
             //preorder walk?
             //inorder walk?
             //postorder walk?
-        
+
         //when you've found the char c, report the path from the root to the node
         //and build the morse code by adding a "." when you go right, "-" when you go left
         return new String("You wish.");
     }
+
+    @Override
     public String toString() {
         return inorderWalk();
     }
-    private String inorderWalk() {  
-        
+    private String inorderWalk() {
+
         return new String("Another wish.");
-    }  
-    
+    }
+
     public static void main(String[] args) {
         MorseTree mt = new MorseTree();  //builds our tree using data from a file
 
         //System.out.println(mt.translate("..."));  //prints out S
         //System.out.println(mt.translate("---"));  //prints out O
         //System.out.println(mt.translate(".......-"));  //prints out null
-        
+
         //System.out.println(mt.translateString("... --- ..."));  //SOS
         //System.out.println(mt.toMorseCode('S'));  //find where we are in the tree, remember path to root
     }
 
     // Inner class to create the linked structure
     private class TreeNode<Character> {
-        
+
         Object data;     // holds a given node’s data
         TreeNode right;
         TreeNode left;
-        
+
         public TreeNode() {
             this.data = null;
             this.right = null;
             this.left = null;
         }
-        
+
         public void setRight(TreeNode rightNode) {
             this.right = rightNode;
         }
-        
+
          public void setLeft(TreeNode leftNode) {
             this.left = leftNode;
         }
-            
+
     }
 }
